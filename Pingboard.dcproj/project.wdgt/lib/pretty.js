c64f6b4623cbb103539a4601dcaefa09c6372c89
Robert Sanders <robert@vitrue.com>
2008-08-08 21:28:59 -0400

    pretty-print dates

---
+++ Pingboard.dcproj/project.wdgt/lib/pretty.js
@@ -0,0 +1,57 @@
+/*
+ * JavaScript Pretty Date
+ * Copyright (c) 2008 John Resig (jquery.com)
+ * Licensed under the MIT license.
+ */
+
+// 2008-01-28T20:24:17Z
+function dateToIso(date)
+{
+    var pad = function(int, digits) {
+         var str = int + "";
+         while (str.length < digits) str = "0" + str;
+         return str;
+      };
+    return date.getUTCFullYear() + '-' + pad(date.getUTCMonth()+1, 2) + '-' + pad(date.getUTCDate(), 2) + 
+           'T' +
+           pad(date.getUTCHours(),2) + ':' + pad(date.getUTCMinutes(),2) + ':' + pad(date.getUTCSeconds(),2) + 'Z';
+}
+
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+function prettyDate(time)
+{
+    var date;
+    if (typeof time == 'object') 
+    {
+        date = time;
+    } else {
+        //time = dateToIso(time);
+
+	    date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+    }
+    var diff = (((new Date()).getTime() - date.getTime()) / 1000),
+	var day_diff = Math.floor(diff / 86400);			
+	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+		return "unknown";
+			
+	return day_diff == 0 && (
+			diff < 60 && "just now" ||
+			diff < 120 && "1 minute ago" ||
+			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+			diff < 7200 && "1 hour ago" ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+		day_diff == 1 && "Yesterday" ||
+		day_diff < 7 && day_diff + " days ago" ||
+		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+}
+
+// If jQuery is included in the page, adds a jQuery plugin to handle it as well
+if ( typeof jQuery != "undefined" )
+	jQuery.fn.prettyDate = function(){
+		return this.each(function(){
+			var date = prettyDate(this.title);
+			if ( date )
+				jQuery(this).text( date );
+		});
+	};
